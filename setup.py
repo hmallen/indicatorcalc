e184fc0f2c13817fa6719a5314d29281a4939216
hmallen <allenhm@gmail.com>
2018-05-24 18:47:43 -0500

    Added installer package.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='indicatorcalc',
+    version='0.1',
+    author='Hunter M. Allen',
+    author_email='allenhm@gmail.com',
+    license='MIT',
+    #packages=find_packages(),
+    packages=['indicatorcalc'],
+    #scripts=['bin/heartbeatmonitor.py'],
+    install_requires=['numpy>=1.2.1',
+                      'TA-Lib>=0.4.17'],
+    description='Centralized, TA-Lib based technical analysis indicator calculation from json market data',
+    keywords=['indicator', 'calc'],
+)
