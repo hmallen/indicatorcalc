c259681901c39efeeedc1d2b188c470e31fa1140
hmallen <allenhm@gmail.com>
2018-05-24 21:49:54 -0500

    Modified dependencies and moved to alpha1.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='indicatorcalc',
-    version='0.1a',
+    version='0.1a1',
     author='Hunter M. Allen',
     author_email='allenhm@gmail.com',
     license='MIT',
     #packages=find_packages(),
     packages=['indicatorcalc'],
     #scripts=['bin/heartbeatmonitor.py'],
-    install_requires=['numpy>=1.2.1',
+    install_requires=['numpy>=1.14.3',
                       'TA-Lib>=0.4.17'],
     description='Centralized, TA-Lib based technical analysis indicator calculation from json market data.',
     long_description=long_description,
