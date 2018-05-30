b98c5bf4fe10e09a8ec60bb4f6604907e738080c
hmallen <allenhm@gmail.com>
2018-05-30 04:32:26 -0500

    Added logging output.

--- indicatorcalc/indicatorcalc.py
+++ indicatorcalc/indicatorcalc.py
@@ -9,7 +9,7 @@ from talib.abstract import EMA, RSI, STOCH
 
 #logging.basicConfig()
 logger = logging.getLogger(__name__)
-#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 
 class IndicatorCalc:
